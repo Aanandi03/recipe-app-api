b7d84ff95aab682486d294037a6299cabe770c43
aanandi03 <aanandirajpara03@gmail.com>
2025-04-29 00:29:57 +0530

    django admin, apidocs

--- app/core/models.py
+++ app/core/models.py
@@ -14,8 +14,14 @@ AbstractBaseUser: (Functionality for auth system)
     - It defines the basic fields and methods that a user should have, such as email, password, and permissions.
     - It also provides a way to authenticate users and check their permissions.
 
+BaseUserManager: Base class for managing users with some helper methods such as
+    normalize_email
+
+PermissionsMixin: Support for django permission system
+                  Includes fields and methods
+
 **extra_fields: can provide keyword argumnets.
-useful when u define additional fields
+useful when u define additional fields (gives the flexibility to create different field in a user - dynamic)
 """
 
 
@@ -24,13 +30,25 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user."""
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError('User must have an email address.')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save(using=self._db)
+
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Create and return a new superuser."""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
 
         return user
 
-# Manager associated to model.
+# Manager associated to model.  self.model = User....User Object
 
 class User(AbstractBaseUser, PermissionsMixin):
     """User in the system."""
@@ -39,4 +57,8 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
+    objects = UserManager() # Creates instance of the manager
+
     USERNAME_FIELD = 'email'
+
+    """ Defines the field that we want to use for authentication (with this we can set our custom default field for user)"""
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -5,7 +5,7 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 """
 get_user_model()  [Helper function for getting the User model]
--> returns the User model that is active in this project.
+-> returns the User model that is active or default in this project.
 -> This is the same as doing from myapp.models import User
 -> Allows referencing the User model by name, rather than by model class.
 -> This is useful because the User model can be changed later on without affecting any code that references it by name.
@@ -30,3 +30,30 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test email is normalized for new users."""
+        sample_emails = [
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
+            ['TEST3@EXAMPLE.COM', 'TEST3@example.com'],
+            ['test4@example.COM', 'test4@example.com'],
+        ]
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, 'sample123')
+            self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """Test that creating a user without an email raises a ValueError."""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'test123')
+
+    def test_create_superuser(self):
+        """Test creating a superuser."""
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'test123',
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
